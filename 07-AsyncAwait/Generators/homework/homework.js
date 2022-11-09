2388cb762a1c631680ff40920056925ce72f763c
pablo vasquez <pablosoleilspo@gmail.com>
2022-11-09 18:36:16 -0500

    07-AsyncAwait

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,20 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
-
+let i = 1;
+while (max ? i <= max : true ) { // si tengo max -> mientras i sea menor o igual a max : -> (sino) dejalo en true
+  if(i % 3 === 0 && i % 5 === 0){
+    yield 'Fizz Buzz'
+  }
+  else if(i % 3 === 0 ){
+    yield 'Fizz'
+  }
+  else if(i % 5 === 0 ){
+    yield 'Buzz'
+  }else{
+    yield i
+  }
+  i++
+}
 }
 
 module.exports = fizzBuzzGenerator;
